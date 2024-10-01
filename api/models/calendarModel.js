281aa638d027ab74766d38700e40d73fa1e328c6
bilgehanakyol <bilgehanakyol12@hotmail.com>
2024-10-01 20:27:26 +0300

    calendar model update

--- api/models/calendarModel.js
+++ api/models/calendarModel.js
@@ -4,7 +4,10 @@ const { Schema } = mongoose;
 const CalendarSchema = new Schema({
   academian: { type: mongoose.Schema.Types.ObjectId, ref: 'Academian', required: true },
   availability: [{
-    day: { type: String, enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], required: true },
+    day: { type: String, 
+      enum: ['Monday', 'Tuesday', 'Wednesday', 
+      'Thursday', 'Friday', 'Saturday', 'Sunday'],
+       required: true },
     slots: [{ 
       start: { type: String, required: true }, 
       end: { type: String, required: true },   
